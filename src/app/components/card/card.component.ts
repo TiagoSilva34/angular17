d1419e46ad4c066e5d3d90633dbc93b4c57244c8
Tiago Borges <tiagoborges899@gmail.com>
2024-06-20 13:10:25 -0300

    feat: implimentações no component de card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,17 +1,85 @@
 import { Component, inject } from '@angular/core';
 import { CoinsService } from '../../services/coins.service';
+import { ICoinProps } from '../../../type';
+import { NgClass, NgIf} from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgIf, FooterComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  coinsService = inject(CoinsService) 
+  coinsService = inject(CoinsService)
+  coinList: ICoinProps[] = [] 
+  showLoader: boolean = false
+  updatedDate: string = ""
+  errorMessage: string = ""
+  // Formula para gerar 3 minutos
+  timer: number = 2 * 60 * 1000 
 
   ngOnInit() {
-    this.coinsService.getCoins().subscribe((coin) => console.log(coin))
+    this.loadCoinsList()
+
+    this.getCurrentTime()
+
+    interval(this.timer).subscribe(() => this.refreshCoinsService())
+  }
+
+  loadCoinsList() {
+    // this.coinsService.getCoins().subscribe((coin) => {
+    //   this.coinList = Object.values(coin).map(item => {
+    //     item.bid = item.bid.slice(0, 4)
+
+    //     if (item.name) {
+    //       let position = item.name.indexOf("/")
+    //       item.name = item.name.slice(0, position)
+    //     }      
+
+    //     return item
+    //   })
+    // })
+
+    this.coinsService.getCoins().subscribe({
+      next: (response) => {
+        this.coinList = Object.values(response).map(item => {
+          item.bid = item.bid.slice(0, 4)
+  
+          if (item.name) {
+            let position = item.name.indexOf("/")
+            item.name = item.name.slice(0, position)
+          }      
+  
+          return item
+        })
+      },
+      error: ({error}) => {
+        this.errorMessage = error.message
+      }
+    })
+  }  
+
+  getCurrentTime() {
+    // Pega a hora atual do windows
+    let date = new Date()
+    let hour = date.getHours()
+    let minute =date.getMinutes()
+    let second = date.getSeconds()
+    this.updatedDate = `${hour}:${minute}:${second}`
+  }
+
+  refreshCoinsService() {
+    this.getCurrentTime()
+
+    this.showLoader = true
+    this.loadCoinsList() 
+
+    // Simula o functionando do loader de carregamento de preço
+    setTimeout(() => {
+      this.showLoader = false
+    }, 1000)
   }
 }
