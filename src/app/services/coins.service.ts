500287129cd2c2228ac3a57f6a90e0fee6e69c9e
Tiago Borges <tiagoborges899@gmail.com>
2024-06-20 11:59:55 -0300

    ajust: ajuste na regras de chamada de API do serviço de moedas

--- src/app/services/coins.service.ts
+++ src/app/services/coins.service.ts
@@ -1,15 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { ICoinProps } from '../../type';
+
+let coinParameter: string = '';
+
+function setNewCoin() {
+  /**
+   * Procedimento para indicar o código de uma moeda, fazer a busca na API e mostrar a moeda no website
+  */
+  const coinCodeList = ['CAD', 'ARS', 'GBP', 'USD'];
+
+  coinCodeList.reduce((a, b) => {
+    return (coinParameter = a + ',' + b);
+  });
+}
+
+setNewCoin()
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoinsService {
-  httpClient = inject(HttpClient)
+  httpClient = inject(HttpClient);
 
-  constructor() { }
+  apiUrl = `https://economia.awesomeapi.com.br/last/${coinParameter}`;
+
+  constructor() {}
 
   getCoins() {
-    return this.httpClient.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    return this.httpClient.get<ICoinProps[]>(this.apiUrl);
   }
 }
